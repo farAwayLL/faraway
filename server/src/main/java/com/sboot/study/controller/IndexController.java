0991271dd87a37d33932aa389a412c7041903459
faraway <13886145909@163.com>
2018-12-03 09:55:28 +0800

    增加indexconreoller

---
+++ server/src/main/java/com/sboot/study/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.sboot.study.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author : faraway
+ * @Date : create in 2018/12/3 9:41
+ * @Description :
+ */
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "<center><h1>欢迎来到浪的飞起个人网！</h1></center>";
+    }
+}
