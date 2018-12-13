72850c68b598183937f2205ad61b79b551751d64
faraway <13886145909@163.com>
2018-12-13 17:56:44 +0800

    添加bigdecimal工具类

--- server/src/main/java/com/sboot/study/utils/BigDecimalUtil.java
+++ server/src/main/java/com/sboot/study/utils/BigDecimalUtil.java
@@ -4,91 +4,77 @@ import java.math.BigDecimal;
 
 /**
  * create by faraway on 2018/12/13
- * description:
+ * description: BigDecimal常用的加减乘除
  */
 public class BigDecimalUtil {
 
     /**
      * 相加
-     *
-     * @param b1
-     * @param b2
-     * @return
      */
     public static BigDecimal add(BigDecimal b1, BigDecimal b2) {
         return b1.add(b2);
     }
 
-    /**
-     * 相减
-     *
-     * @param b1
-     * @param b2
-     * @return
-     */
+    /**相减*/
     public static BigDecimal subtract(BigDecimal b1, BigDecimal b2) {
         return b1.subtract(b2);
     }
 
-    /**
-     * 相乘
-     *
-     * @param b1
-     * @param b2
-     * @return
-     */
+    /**相乘*/
     public static BigDecimal multiply(BigDecimal b1, BigDecimal b2) {
         return b1.multiply(b2);
     }
 
-    /**
-     * 相除 能整除返回商，不能整除保留两位小数
-     *
-     * @param b1
-     * @param b2
-     * @return
-     */
+    /**相除 能整除返回商，不能整除返回保留两位小数的商*/
     public static BigDecimal divide(BigDecimal b1, BigDecimal b2) {
-        //求商和余数 bigDecimals[0]是商，bigDecimals[1]是余数
-        BigDecimal[] bigDecimals = b1.divideAndRemainder(b2);
-        //如果能被整除，直接返回
-        if (bigDecimals[1] == BigDecimal.valueOf(0)) {
-            return b1.divide(b2).stripTrailingZeros();
+        BigDecimal bigDecimal = remainder(b1, b2);
+        //余数为0，说明能被整除，直接返回
+        if (bigDecimal == BigDecimal.valueOf(0)) {
+            return b1.divide(b2);
         } else {
-            //保留两位小数,四舍五入
+            //如果不能被整除     保留两位小数           并四舍五入
             return b1.divide(b2, 2, BigDecimal.ROUND_HALF_UP);
         }
     }
 
-    /**
-     * 相除 返回商和余数的数组 [0]是商，[1]是余数
-     *
-     * @param b1
-     * @param b2
-     * @return
-     */
+    /**相除 返回余数*/
+    public static BigDecimal remainder(BigDecimal b1, BigDecimal b2) {
+        return b1.remainder(b2);
+    }
+
+    /**相除 返回商和余数的数组 [0]是商，[1]是余数*/
     public static BigDecimal[] divideAndRemainder(BigDecimal b1, BigDecimal b2) {
-        //返回商和余数 bigDecimals[0]是商，bigDecimals[1]是余数
         return b1.divideAndRemainder(b2);
     }
 
+    /**比较大小 返回值为-1 0 1   -1表示b1比b2小，0表示相等，1表示b1比b2大*/
+    public static int compareTo(BigDecimal b1, BigDecimal b2) {
+        return b1.compareTo(b2);
+    }
+
+    /**求绝对值*/
+    public static BigDecimal abs(BigDecimal b) {
+        return b.abs();
+    }
+
+    /**求相反数*/
+    public static BigDecimal negate(BigDecimal b) {
+        return b.negate();
+    }
 
     /**
-     * setScale()方法用于格式化小数点
-     * setScale(1, BigDecimal.ROUND_DOWN);       //直接删除多余的小数位，如2 .369 会变成2 .3
-     * setScale(1, BigDecimal.ROUND_UP);         //进位处理，如2 .3*，都会变成2 .4
-     * setScale(1, BigDecimal.ROUND_HALF_UP);    //四舍五入，如2.45，会变成2.5
-     * setScale(1, BigDecimal.ROUND_HALF_DOWN);  //五舍六入，如2.45，会变成2.4
+     * bigDecimal.setScale()方法用于格式化小数点
+     * BigDecimal b = setScale(1, BigDecimal.ROUND_DOWN);       //直接删除多余的小数位，如2 .369 会变成2 .3
+     * BigDecimal b = setScale(1, BigDecimal.ROUND_UP);         //进位处理，如2 .3*，都会变成2 .4
+     * BigDecimal b = setScale(1, BigDecimal.ROUND_HALF_UP);    //四舍五入，如2.45，会变成2.5
+     * BigDecimal b = setScale(1, BigDecimal.ROUND_HALF_DOWN);  //五舍六入，如2.45，会变成2.4
      */
 
-//    public static void main(String[] args) {
-//        BigDecimal b1 = new BigDecimal(9);
-//        BigDecimal b2 = new BigDecimal(3);
-//        System.out.println(add(b1, b2));
-//        System.out.println(subtract(b1, b2));
-//        System.out.println(multiply(b1, b2));
-//        System.out.println(divide(b1, b2));
-//        System.out.println(divide(b2, b1));
-//        System.out.println(divideAndRemainder(b1, b2)[0] + "..." + divideAndRemainder(b1, b2)[1]);
-//    }
+    /**
+     * BigDecimal b = bigDecimal.stripTrailingZeros()方法用于去除小数点末尾的0，保留有效小数位
+     * <p>
+     * String str = bigDecimal.toString()转成字符串，但不识别科学记数法     1E4---->1E4
+     * String str = bigDecimal.toPlainString()识别科学计数法并转成字符串   1E4---->10000
+     */
+
 }
