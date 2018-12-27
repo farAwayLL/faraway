7a7d1c74e266d2342d99af7b7707e7ad1743ab52
faraway <13886145909@163.com>
2018-12-27 17:21:52 +0800

    定时任务

--- server/src/main/java/com/sboot/study/ServerApplication.java
+++ server/src/main/java/com/sboot/study/ServerApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -11,10 +12,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @date: 2018/11/8 9:43
  * @description:
  */
-@SpringBootApplication
-@MapperScan(basePackages = "com.sboot.study.mybatisMapper")
-@EnableSwagger2
-@ImportResource(locations = "classpath:spring/spring-jdbc.xml")
+@SpringBootApplication//启动入口
+@MapperScan(basePackages = "com.sboot.study.mybatisMapper")//扫描mapper地址
+@EnableSwagger2//开启swagger
+@ImportResource(locations = "classpath:spring/spring-jdbc.xml")//导入配置文件
+@EnableScheduling//开启任务调度
 public class ServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
---
+++ server/src/main/java/com/sboot/study/scheduler/SchedulerTest.java
@@ -0,0 +1,24 @@
+package com.sboot.study.scheduler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * create by faraway on 2018/12/27
+ * description: 定时任务
+ */
+//非@Controller @Service就用该通用注解
+@Component
+public class SchedulerTest {
+
+    private static final Logger log = LoggerFactory.getLogger(SchedulerTest.class);
+
+    @Scheduled(cron = "0/10 * * * * ?")
+    public void sysout() {
+        System.out.println("0.0.....");
+    }
+
+
+}
