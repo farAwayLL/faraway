281667ccea22c85a1cd540b3ff7f98037f40f954
faraway <13886145909@163.com>
2019-01-11 18:15:00 +0800

    添加保留四位有效小数位

--- server/src/main/java/com/sboot/study/utils/BigDecimalUtil.java
+++ server/src/main/java/com/sboot/study/utils/BigDecimalUtil.java
@@ -1,7 +1,6 @@
 package com.sboot.study.utils;
 
 import java.math.BigDecimal;
-import java.text.DecimalFormat;
 
 /**
  * create by faraway on 2018/12/13
@@ -16,17 +15,23 @@ public class BigDecimalUtil {
         return b1.add(b2);
     }
 
-    /**相减*/
+    /**
+     * 相减
+     */
     public static BigDecimal subtract(BigDecimal b1, BigDecimal b2) {
         return b1.subtract(b2);
     }
 
-    /**相乘*/
+    /**
+     * 相乘
+     */
     public static BigDecimal multiply(BigDecimal b1, BigDecimal b2) {
         return b1.multiply(b2);
     }
 
-    /**相除 能整除直接返回，不能整除四舍五入保留两位小数*/
+    /**
+     * 相除 能整除直接返回，不能整除四舍五入保留两位小数
+     */
     public static BigDecimal divide(BigDecimal b1, BigDecimal b2) {
         BigDecimal bigDecimal = remainder(b1, b2);
         //余数为0，说明能被整除，直接返回
@@ -38,35 +43,49 @@ public class BigDecimalUtil {
         }
     }
 
-    /**相除 返回余数*/
+    /**
+     * 相除 返回余数
+     */
     public static BigDecimal remainder(BigDecimal b1, BigDecimal b2) {
         return b1.remainder(b2);
     }
 
-    /**相除 返回商和余数的数组 [0]是商，[1]是余数*/
+    /**
+     * 相除 返回商和余数的数组 [0]是商，[1]是余数
+     */
     public static BigDecimal[] divideAndRemainder(BigDecimal b1, BigDecimal b2) {
         return b1.divideAndRemainder(b2);
     }
 
-    /**比较大小 返回值为-1 0 1   -1表示b1比b2小，0表示相等，1表示b1比b2大*/
+    /**
+     * 比较大小 返回值为-1 0 1   -1表示b1比b2小，0表示相等，1表示b1比b2大
+     */
     public static int compareTo(BigDecimal b1, BigDecimal b2) {
         return b1.compareTo(b2);
     }
 
-    /**求绝对值*/
+    /**
+     * 求绝对值
+     */
     public static BigDecimal abs(BigDecimal b) {
         return b.abs();
     }
 
-    /**求相反数*/
+    /**
+     * 求相反数
+     */
     public static BigDecimal negate(BigDecimal b) {
         return b.negate();
     }
 
-    /**保留四位有效小数位 例：10.1000 -->10.1; 0.0030 -->0.003; 10.0000 -->10*/
+    /**
+     * 保留四位有效小数位 例：10.1000 -->10.1; 0.0030 -->0.003; 10.0000 -->10
+     */
     public static BigDecimal format(BigDecimal b) {
-        DecimalFormat decimalFormat = new DecimalFormat("############.####");
-        return new BigDecimal(decimalFormat.format(b));
+        return b.stripTrailingZeros();
+        //或者
+        //DecimalFormat decimalFormat = new DecimalFormat("############.####");
+        //return new BigDecimal(decimalFormat.format(b));
     }
 
     /**
